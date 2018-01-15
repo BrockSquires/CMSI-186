f98d6be6528bf07f8742cc02b23746f7c3a03915
BrockSquires <35274653+BrockSquires@users.noreply.github.com>
2018-01-15 15:15:26 -0800

    Helloworld
    This is the first upload of the first homework for cmsi 186

---
+++ homework00/Helloworld.java
@@ -0,0 +1,5 @@
+public class Helloworld {  //this sets the class
+  public static void main( String[] args ) { // this is also just for setup
+    System.out.println( "Hello, world!" ); // this prints "Hello world"
+  }
+}
